d9f96a91e73da8b33de22769b531cd69e594f6e0
DanielS262 <60705930+DanielS262@users.noreply.github.com>
2022-03-03 11:25:15 -0300

    Add files via upload

--- backend/routes.js
+++ backend/routes.js
@@ -2,9 +2,16 @@ const express = require('express')
 
 const route = express.Router()
 const funcionarioControll = require("./src/controller/funcionarioControll")
+const assistidoControll = require('./src/controller/assistidoControll')
 
 route.get('/funcionarios', funcionarioControll.getAll)
 route.get('/funcionarios/:id_funcionario', funcionarioControll.getID)
 route.post('/funcionarios/', funcionarioControll.postFuncionario)
+route.put('/funcionarios', funcionarioControll.updateFuncionario)
+route.delete('/funcionarios/:id_funcionario', funcionarioControll.deletarFuncionario)
+
+route.get('/assistidos', assistidoControll.getAll)
+route.get('/assistidos/:id_assistido', assistidoControll.getID)
+route.post('/assistidos', assistidoControll.postAssistido)
 
 module.exports = route
